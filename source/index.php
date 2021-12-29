bf4c7444e652f61526041b3ffede77393c6267d4
root <root@ip-172-31-31-93.eu-central-1.compute.internal>
2021-12-29 09:32:24 +0000

    first commit

---
+++ source/index.php
@@ -0,0 +1,19 @@
+<?php
+// Символы, которые будут использоваться в пароле.
+$chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP";
+// Количество символов в пароле.
+$max=10;
+// Определяем количество символов в $chars
+$size=StrLen($chars)-1;
+// Определяем пустую переменную, в которую и будем записывать символы.
+$password=null;
+// Создаём пароль.
+    while($max--)
+    $password.=$chars[rand(0,$size)];
+// Выводим созданный пароль.
+echo
+"<center>
+Сгенерированный пароль:
+<hr><font face=verdana color=red size=7><b>".$password."</b></font><hr>
+<a href=&#63;>Создать новый пароль.</a></center>";
+?>
